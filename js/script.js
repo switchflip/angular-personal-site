9f8e6de16b2175303fe518ac92e56fbaebbcba3c
Thomas Berry <thom.berry@gmail.com>
2014-09-05 00:29:03 -0400

    added angular ui router

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// app.js
+var personalSite = angular.module('personalSite', ['ui.router']);
+
+
+personalSite.config(["$locationProvider", function($locationProvider) {
+  $locationProvider.html5Mode(true);
+}]);
+
+personalSite.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/',
+            templateUrl: '/pages/home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('work', {
+            url: '/work',
+            templateUrl: '/pages/work.html'
+        })
+
+        .state('about', {
+            url: '/about',
+            templateUrl: '/pages/about.html'
+        })
+
+        .state('contact', {
+            url: '/contact',
+            templateUrl: '/pages/contact.html'
+        });
+        
+});

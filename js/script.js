0f8eee11b49d3435f2ade2555d80d35a83a7ae46
Thomas Berry <thom.berry@gmail.com>
2014-09-05 00:47:03 -0400

    Working on angular ui router

--- js/script.js
+++ js/script.js
@@ -8,30 +8,29 @@ personalSite.config(["$locationProvider", function($locationProvider) {
 
 personalSite.config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/');
+    // $urlRouterProvider.otherwise('/');
     
     $stateProvider
         
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/',
-            templateUrl: '/pages/home.html'
+            templateUrl: 'pages/home.html'
         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('work', {
             url: '/work',
-            templateUrl: '/pages/work.html'
+            templateUrl: 'pages/work.html'
         })
 
         .state('about', {
             url: '/about',
-            templateUrl: '/pages/about.html'
+            templateUrl: 'pages/about.html'
         })
 
         .state('contact', {
             url: '/contact',
-            templateUrl: '/pages/contact.html'
+            templateUrl: 'pages/contact.html'
         });
-        
 });
